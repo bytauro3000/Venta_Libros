b472bcfc53ca8a77aa4587d6423efd363047073f
Ever <Bytauro2016@gmail.com>
2025-06-08 22:54:28 -0500

    Comit10

--- Venta_Libros/Models/Clientes.cs
+++ Venta_Libros/Models/Clientes.cs
@@ -7,6 +7,7 @@ namespace Venta_Libros.Models
 {
     public class Clientes
     {
+        //Nuevas modificaciones
         private int id {  get; set; }
         private string name { get; set; }
         private string apellidos {  get; set; }
