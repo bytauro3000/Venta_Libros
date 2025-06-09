31b6122aacd932680e3ee3e18b77334eab723ca8
Ever <Bytauro2016@gmail.com>
2025-06-08 22:56:38 -0500

    EVER SILVA CAMPOS

--- Venta_Libros/Models/Clientes.cs
+++ Venta_Libros/Models/Clientes.cs
@@ -7,7 +7,7 @@ namespace Venta_Libros.Models
 {
     public class Clientes
     {
-        //Nuevas modificaciones
+        //EVER SILVA CAMPOS
         private int id {  get; set; }
         private string name { get; set; }
         private string apellidos {  get; set; }
