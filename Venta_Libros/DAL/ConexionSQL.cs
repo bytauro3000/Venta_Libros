a97d54b1fdb159cd27004234df695b747ede8ceb
Ever <Bytauro2016@gmail.com>
2025-06-09 00:17:15 -0500

    se agrego conexion a base de datos

---
+++ Venta_Libros/DAL/ConexionSQL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Venta_Libros.DAL
+{
+    public class ConexionSQL
+    {
+
+        private readonly string connectionString = ConfigurationManager.ConnectionStrings["Libreria"].ConnectionString;
+
+        public string ProbarConexion()
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                {
+                    cn.Open();
+                    return "✅ Conexión exitosa a la base de datos.";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "❌ Error al conectar: " + ex.Message;
+            }
+        }
+    }
+}
