20a4fbb07b5bd0e3b7d076d9460afaad422f1743
Ever <Bytauro2016@gmail.com>
2025-06-08 21:01:27 -0500

    commit 7

---
+++ Venta_Libros/Models/Clientes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Venta_Libros.Models
+{
+    public class Clientes
+    {
+        private int id {  get; set; }
+        private string name { get; set; }
+        private string apellidos {  get; set; }
+
+    }
+}
