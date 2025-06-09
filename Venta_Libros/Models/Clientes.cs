4835e31e0d5e7c362920cefbf18b1af5772484c1
Ever <Bytauro2016@gmail.com>
2025-06-08 22:59:30 -0500

    modificacion 22.59

--- Venta_Libros/Models/Clientes.cs
+++ Venta_Libros/Models/Clientes.cs
@@ -7,7 +7,7 @@ namespace Venta_Libros.Models
 {
     public class Clientes
     {
-        //EVER SILVA CAMPOS
+        // ES HOY EL DIA
         private int id {  get; set; }
         private string name { get; set; }
         private string apellidos {  get; set; }
